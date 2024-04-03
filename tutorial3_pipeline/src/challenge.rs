02335849279810182fb94abdbd45adc063281443
Jeklah <arthurbowers@gmail.com>
2024-04-03 20:22:35 +0100

    Finishing off challenge  although doesn't work stilll.

--- tutorial3_pipeline/src/challenge.rs
+++ tutorial3_pipeline/src/challenge.rs
@@ -13,7 +13,7 @@ struct State {
     config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
     render_pipeline: wgpu::RenderPipeline,
-    challenge_render_pipline: wgpu::RenderPipeline,
+    challenge_render_pipeline: wgpu::RenderPipeline,
     use_color: bool,
     window: Window,
 }
@@ -133,7 +133,7 @@ impl State {
                 count: 1,
                 mask: !0,
                 alpha_to_coverage_enabled: false,
-            };
+            },
             // If the pipeline will be used with a multiview render pass, this
             // indicates how many array layers the attachments will have.
             multiview: None,
@@ -224,7 +224,7 @@ impl State {
                     },
                 ..
             } => {
-                sekf,yse)cikir - *state == ElementState::Released;
+                self.use_color = *state == ElementState::Released;
                 true
             }
             _ => false,
@@ -238,23 +238,110 @@ impl State {
         let view = output
             .texture
             .create_view(&wgpu::TextureViewDescriptor::default());
+
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+    
+        {
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                occlusion_query_set: None,
+                timestamp_writes: None,
+            });
+
+            render_pass.set_pipeline(if self.use_color {
+                &self.render_pipeline
+            } else {
+                &self.challenge_render_pipeline
+            });
+            render_pass.draw(0..3, 0..1);
+        }
+
+        self.queue.submit(iter::once(encoder.finish()));
+        output.present();
+
+        Ok(())
     }
+}
 
+fn fn main() {
+    pollster::block_on(run());
+}
 
+async fn run() {
+    env_logger::init();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .build(&event_loop)
+        .unwrap(); // Unwrap because we're not handling the error here
 
+    let mut state = State::new(window).await;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
+    event_loop.run(move |event, _, control_flow| {
+        match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == state.window().id() => {
+                if !state.input(event) {
+                    match event {
+                        WindowEvent::CloseRequested
+                            | WindowEvent::KeyboardInput {
+                                input:
+                                    KeyboardInput {
+                                        state: ElementState::Released,
+                                        virtual_keycode: Some(VirtualKeyCode::Escape),
+                                        ..
+                                    },
+                                ..
+                            } => *control_flow = ControlFlow::Exit,
+                        WindowEvent::Resized(physical_size) => {
+                            state.resize(physical_size);
+                        }
+                        WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                            state.resize(**new_inner_size);
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            Event::RedrawRequested(window_id) if window_id == state.window().id() => {
+                state.update();
+                match state.render() {
+                    OK(_) => {}
+                    // Reconfigur the surface if lost
+                    Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
+                        state.resize(state.size);
+                    }
+                    // The system is out of memory, we should probably quit
+                    Err(wgpu::SurfaceError::OutOfMemory) => {
+                        *control_flow = ControlFlow::Exit;
+                    }
+                    // We're ignoring timeous
+                    Err(wgpu::SurfaceError::Timeout) => log::warn!("Surface Timeout"),
+                }
+            }
+            Event::MainEventsCleared => {
+                state.window().request_redraw();
+            }
+            _ => {}
+        }
+    });    
 }

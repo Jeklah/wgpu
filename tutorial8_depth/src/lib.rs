d2e1b0f155b7ddfff2bf322898dbb7a08e3c541e
arthur bowers <arthur.bowers@phabrix.com>
2024-05-28 16:44:01 +0100

    Adding challenge.frag and starting implementation of lib.rs

---
+++ tutorial8_depth/src/lib.rs
@@ -0,0 +1,94 @@
+use std::iter;
+
+use cgmath::prelude::*;
+use wgpu::util::DeviceExt;
+use winit::{
+    event::*,
+    event_loop::EventLoop,
+    keyboard::{KeyCode, PhysicalKey},
+    window::{Window, WindowBuilder},
+};
+
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+mod texture;
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    tex_coords: [f32; 2],
+}
+
+impl Vertex {
+    fn desc() -> wgpu::VertexBufferLayout<'static> {
+        use std::mem;
+        wgpu::VertexBufferLayout {
+            array_stride: mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &[
+                wgpu::VertexAttribute {
+                    offset: 0,
+                    shader_location: 0,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+                wgpu::VertexAttribute {
+                    offset: mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    shader_location: 1,
+                    format: wgpu::VertexFormat::Float32x2,
+                },
+            ],
+        }
+    }
+}
+
+const VERTICES: &[Vertex] = &[
+    Vertex {
+        position: [-0.0868241, -0.49240386, 0.0],
+        tex_coords: [0.4131759, 0.00759614],
+    },
+    Vertex {
+        position: [-0.49513406, 0.06958647, 0.0],
+        tex_coords: [0.0048659444, 0.43041354],
+    },
+    Vertex {
+        position: [-0.21918549, 0.44939706, 0.0],
+        tex_coords: [0.28081453, 0.949397],
+    },
+    Vertex {
+        position: [0.35966998, 0.3473291, 0.0],
+        tex_coords: [0.85967, 0.84732914],
+    },
+    Vertex {
+        position: [0.44147372, -0.2347359, 0.0],
+        tex_coords: [0.9414737, 0.2652641],
+    },
+];
+
+const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4, /* padding */ 0];
+
+#[rustfmt::skip]
+pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
+    1.0,  0.0, 0.0, 0.0,
+    0.0,  1.0, 0.0, 0.0,
+    0.0,  0.0, 0.5, 0.5,
+    0.0,  0.0, 0.0, 1.0,
+);
+
+const NUM_INSTANCES_PER_ROW: u32 = 10;
+const INSTANCE_DISPLACEMENT: cgmath::Vector3<f32> = cgmath::Vector3::new(
+    NUM_INSTANCES_PER_ROW as f32 * 0.5,
+    0.0,
+    NUM_INSTANCES_PER_ROW as f32 * 0.5,
+);
+
+struct Camera {
+    eye: cgmath::Point3<f32>,
+    target: cgmath::Point3<f32>,
+    up: cgmath::Vector3<f32>,
+    aspect: f32,
+    fovy: f32,
+    znear: f32,
+    zfar: f32,
+}

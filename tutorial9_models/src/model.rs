d2b01746174b97018b0872e1cb2686431b59a470
arthur bowers <arthur.bowers@phabrix.com>
2024-06-06 16:30:26 +0100

    Initial commit for tutorial 9

---
+++ tutorial9_models/src/model.rs
@@ -0,0 +1,19 @@
+// model.rs
+
+pub trait Vertex {
+    fn desc() -> wgpu::VertexBufferLayout<'static>;
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+pub struct ModelVertex {
+    pub position: [f32; 3],
+    pub tex_coords: [f32; 2],
+    pub normal: [f32; 3],
+}
+
+impl Vertex for ModelVertex {
+    fn desc() -> wgpu::VertexBufferLayout<'static> {
+        todo!();
+    }
+}

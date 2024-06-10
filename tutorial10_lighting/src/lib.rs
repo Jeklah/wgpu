5f123fc85622da9f7afa26cc2d7881e32c53b8a0
arthur bowers <arthur.bowers@phabrix.com>
2024-06-10 16:09:42 +0100

    Starting implementation of lib.rs

---
+++ tutorial10_lighting/src/lib.rs
@@ -0,0 +1,68 @@
+use std::iter;
+
+use cgmath::prelude::*;
+use wgpu::util::DeviceExt;
+use winit::{
+    event::*,
+    event_loop::EventLoop,
+    keyboard::{KeyCode, PhysicalKey},
+    window::Window,
+};
+
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+mod model;
+mod resources;
+mod texture;
+
+use model::{DrawLight, DrawModel, Vertex};
+
+#[rustfmt::skip]
+pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
+   1.0,  0.0, 0.0, 0.0, 
+   0.0,  1.0, 0.0, 0.0,
+   0.0,  0.0, 0.5, 0.0,
+   0.0,  0.0, 0.0, 1.0,
+);
+
+const NUM_INSTANCES_PER_ROW: u32 = 10;
+
+struct Camera {
+    eye: cgmath::Point3<f32>,
+    target: cgmath::Point3<f32>,
+    up: cgmath::Vector3<f32>,
+    aspect: f32,
+    fovy: f32,
+    znear: f32,
+    zfar: f32,
+}
+
+impl Camera {
+    fn build_view_projection_matrix(&self) -> cgmath::Matrix4<f32> {
+        let view = cgmath::Matrix4::look_at_rh(self.eye, self.target, self.up);
+        let proj = cgmath::perspective(cgmath::Deg(self.fovy), self.aspect, self.znear, self.zfar);
+        proj * view
+    }
+}
+
+#[repr(C)]
+#[derive(Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+struct CameraUniform {
+    view_position: [f32; 4],
+    view_proj: [[f32; 4]; 4],
+}
+
+impl CameraUniform {
+    fn new() -> Self {
+        Self {
+            view_position: [0.0; 4],
+            view_proj: cgmath::Matrix4::identity().into(),
+        }
+    }
+
+    fn update_view_proj(&mut self, camera: &Camera) {
+        self.view_position = camera.eye.to_homogeneous().into();
+        self.view_proj = camera.build_view_projection_matrix().into();
+    }
+}
